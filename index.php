6ec369a3c9e5e2b502e492d8eddcefd36416b867
gisandaa <aliyagisanda@gmail.com>
2021-11-02 11:27:12 +0700

    index

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $subscriber = $result['items'][0]['statistics']['subscriberCount'];
 <!doctype html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
+    
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
